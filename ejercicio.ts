56093417f113e3a2638bf12982dc186b8d80708c
Francisco Rivero <franciscorivero0@gmail.com>
2023-06-20 22:58:10 -0300

    b

--- ejercicio.ts
+++ ejercicio.ts
@@ -1,12 +1,18 @@
-let sonido:string="miau"
 class Animal {
+    sonido
+    constructor(sonido:string){
+        this.sonido= sonido
+    }
+    
     hacersonido(){
-        return console.log(sonido)
+        return console.log(this.sonido)
     }
 }
 class Gato extends Animal{
-  
-}
+    
+    }
+let gato1= new Gato("miau")
 class Perro extends Animal{
 
 }
+gato1.hacersonido()
