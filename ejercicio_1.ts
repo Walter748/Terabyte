83baed4587287aab807e4bf913ad5ab864aca3d8
Walter <waltertrentacosta8@gmail.com>
2023-06-22 22:05:14 -0300

    Ejercicio_1_Terminado

--- ejercicio_1.ts
+++ ejercicio_1.ts
@@ -1,7 +1,11 @@
 class Animal {
     sonido
-    constructor(sonido:string){
-        this.sonido= sonido
+    raza
+    edad
+    constructor(sonido:string, raza:string, edad:number){
+        this.sonido = sonido
+        this.raza = raza
+        this.edad = edad
     }
     
     hacersonido(){
@@ -9,12 +13,16 @@ class Animal {
     }
 }
 class Gato extends Animal{
-    
+    hacersonido(){
+        return console.log(this.sonido)
+    }
     }
 class Perro extends Animal{
-
+    hacersonido(){
+        return console.log(this.sonido)
+    }
 }
-let gato1= new Gato("miau")
-let perro1= new Perro ("Guau")
+let gato1= new Gato("miau", "Siames", 5)
+let perro1= new Perro ("Guau", "Pastor Aleman", 1)
 gato1.hacersonido()
 perro1.hacersonido()
