ae1d412a866a401f9e1a5175de43ba2126b15213
Dantesssssuwu <danttramirez137@gmail.com>
2023-06-22 22:59:46 -0300

    finalizado

--- ejercio_2.ts
+++ ejercio_2.ts
@@ -2,7 +2,7 @@ interface lista {
     completada:Boolean[];
     id:number[];
 }
-class traeas implements lista {
+class tareas implements lista {
     completada: Boolean[];
     id: number[];
     private listaTareas:string[]
@@ -16,4 +16,22 @@ class traeas implements lista {
         this.id.push(ids);
         this.completada.push(false);
     }
+    CompletarTarea(id:number):void{
+        let indice = this.id.indexOf(id);
+        this.completada[indice] = true;
+    }
+    mostraTareas(): void{
+        for (let i in this.listaTareas)
+            console.info( 'id:'+this.id[i]+' la tareas es: '+this.listaTareas[i]+' el valor es: '+this.completada[i])
+    }
+
+}
+let tarea = new tareas()
+
+tarea.Agregar('limpiar la pieza ', 10)
+tarea.Agregar('limpiar la cocina ', 11)
+
+tarea.CompletarTarea(10)
+
+tarea.mostrarTareas()
 }
